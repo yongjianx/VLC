e8cabc25831244101ec15a9454041f214e74e309
SkyworthClub <skyworthclub@SkyworthClubdeMacBook-Pro.local>
2018-06-20 21:48:42 +0800

    new project from byacht

---
+++ app/src/main/java/ht/thread/DaemonThreadFactory.java
@@ -0,0 +1,24 @@
+package ht.thread;
+
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Created by dn on 2018/6/17.
+ */
+
+public class DaemonThreadFactory implements ThreadFactory, Thread.UncaughtExceptionHandler {
+
+    @Override
+    public Thread newThread(@NonNull Runnable r) {
+        Thread thread = new Thread(r);
+        thread.setDaemon(true);
+        return thread;
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("caught: "+ e);
+    }
+}
---
+++ app/src/main/java/ht/thread/ImageThread.java
@@ -0,0 +1,116 @@
+package ht.thread;
+
+import org.opencv.core.Mat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+import ht.activity.LocationActivity;
+import ht.bean.Coordinate;
+
+/**
+ * Created by dn on 2018/6/17.
+ */
+
+public class ImageThread {
+
+    private ExecutorService mExecutorService;
+    private FutureTask<Integer> mFutureTask;
+    private List<Integer> mLedLineList = new ArrayList<>();
+
+    public ImageThread(){
+        mExecutorService = Executors.newCachedThreadPool(new DaemonThreadFactory());
+    }
+
+    public void handlePic(){
+        mExecutorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                if (handlePicture != null) {
+                    handlePicture.handle();
+                }
+            }
+        });
+    }
+
+    public List<Integer> getLedLineCount(List<Mat> imgs){
+        mLedLineList.clear();
+        final CountDownLatch countDownLatch = new CountDownLatch(3);
+        for (final Mat img:imgs){
+            Callable<Integer> callable = new Callable<Integer>() {
+                @Override
+                public Integer call() throws Exception {
+                        return handlePicture.getCount(img);
+                }
+            };
+
+            mFutureTask = new FutureTask<Integer>(callable){
+                @Override
+                protected void done() {
+                    try{
+                        mLedLineList.add(mFutureTask.get());
+                        countDownLatch.countDown();
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }catch (ExecutionException e){
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            //启动任务
+            mExecutorService.execute(mFutureTask);
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            return mLedLineList;
+        }
+
+    }
+
+    public void isCollinear(final List<Coordinate> X, final List<Coordinate> Y) {
+        mExecutorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                if (handlePicture != null) {
+                    handlePicture.isCollinear(X, Y);
+                    getLocation();
+                }
+            }
+        });
+    }
+
+    public void getLocation() {
+        mExecutorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                if (handlePicture != null) {
+                    handlePicture.getLocation();
+                }
+            }
+        });
+    }
+
+    public interface HandlerPicture {
+        void handle();
+        int getCount(Mat img);
+        void isCollinear(List<Coordinate> X, List<Coordinate> Y);
+        void getLocation();
+    }
+
+    HandlerPicture handlePicture;
+
+    public void setHandlePictureListener(HandlerPicture handlePicture) {
+        this.handlePicture = handlePicture;
+    }
+
+}
